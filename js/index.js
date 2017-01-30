259530d834cf387c41a8963352ce950799e65d3d
BorutaBartosz <bboruta.dev@gmail.com>
2017-01-30 19:24:01 +0100

    created function to render binaries

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-  
+  renderBinaryArray(1023);
 })
+
+function renderBinaryArray(value) {
+  var binaries = [];
+  for (var i = 0; i <= value; i++) {
+    if(dec2bin(i).length < 11) {
+      binaries.push(addZero(10 - dec2bin(i).length) + ''+ dec2bin(i));
+    }
+  }
+  return binaries;
+}
+
+function dec2bin(dec) {
+    return (dec >>> 0).toString(2);
+}
+
+function addZero(length) {
+    var missing = '';
+    for (var i = 0; i<length; i++) {
+      missing +='0';
+    }
+    return missing;
+}
