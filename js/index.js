f4f997517ba07d5197928bc66c2fb9507cb00b1f
BorutaBartosz <bboruta.dev@gmail.com>
2017-01-30 20:48:07 +0100

    function to render emptyarrays and function to fill them with values

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  renderBinaryArray(1023);
+  fillQuadraWithValues(renderBinaryArray(1023));
 })
 
 function renderBinaryArray(value) {
@@ -22,4 +22,34 @@ function addZero(length) {
       missing +='0';
     }
     return missing;
+}
+
+function renderQuadArray() {
+  var emptyArr = new Array(5);
+  for (var i = 0; i < 5; i++) {
+    emptyArr[i] = new Array(5);
+  }
+  return emptyArr;
+}
+
+function fillQuadraWithValues(binnaryArray) {
+  var container = [];
+  _.each(binnaryArray, function(bin, key) {
+    var arr = renderQuadArray();
+      for (var i = 0; i < 5; i++) {
+        arr[i][i] = '0';
+      }
+        arr[0][1] = arr[1][0] = bin[0];
+        arr[0][2] = arr[2][0] = bin[1];
+        arr[0][3] = arr[3][0] = bin[2];
+        arr[0][4] = arr[4][0] = bin[3];
+        arr[1][2] = arr[2][1] = bin[4];
+        arr[1][3] = arr[3][1] = bin[5];
+        arr[1][4] = arr[4][1] = bin[6];
+        arr[2][3] = arr[3][2] = bin[7];
+        arr[2][4] = arr[4][2] = bin[8];
+        arr[3][4] = arr[4][3] = bin[9];
+        container.push(arr);
+  })
+  return container;
 }
