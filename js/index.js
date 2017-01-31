71ca4aa58bd37848efe8478bae6d940315d96bb9
BorutaBartosz <bboruta.dev@gmail.com>
2017-01-31 20:08:10 +0100

    generating and showing arrays

--- js/index.js
+++ js/index.js
@@ -30,28 +30,27 @@ function addZero(length) {
 function renderQuadArray() {
   var emptyArr = new Array(5);
   for (var i = 0; i < 5; i++) {
-    emptyArr[i] = new Array(5);
+    emptyArr[i] = Array(5+1).join('0').split('');
   }
   return emptyArr;
 }
 
 function fillQuadraWithValues(binnaryArray) {
   var container = [];
+
   _.each(binnaryArray, function(bin, key) {
+    iterator = 0;
     var arr = renderQuadArray();
-      for (var i = 0; i < 5; i++) {
-        arr[i][i] = '0';
+      for(row in arr){
+        for(col in arr){
+          if(col > row) {
+            arr[row][col] = bin[iterator];
+            arr[col][row] = bin[iterator];
+            iterator+=1;
+          }
+        }
       }
-        arr[0][1] = arr[1][0] = bin[0];
-        arr[0][2] = arr[2][0] = bin[1];
-        arr[0][3] = arr[3][0] = bin[2];
-        arr[0][4] = arr[4][0] = bin[3];
-        arr[1][2] = arr[2][1] = bin[4];
-        arr[1][3] = arr[3][1] = bin[5];
-        arr[1][4] = arr[4][1] = bin[6];
-        arr[2][3] = arr[3][2] = bin[7];
-        arr[2][4] = arr[4][2] = bin[8];
-        arr[3][4] = arr[4][3] = bin[9];
+
         container.push(arr);
   })
   return container;
@@ -62,15 +61,31 @@ function submitInputs() {
   for(var i = 0; i < 5; i++) {
     vals.push(parseInt($('#in'+i).val()));
   }
-
   var amout = _.sortBy(vals);
   compareArrays(summedRows, amout);
 }
 
-function compareArrays(arr1, arr2){
-  _.each(arr1, function(val, key){
-    if(_.isEqual(val, arr2) === true) console.log(tab[key]);
+function compareArrays(arr1, arr2) {
+  finalArr = [];
+  _.each(arr1, function(val, key) {
+    if(_.isEqual(val, arr2) === true) finalArr.push(tab[key]);
   })
+  printFinalArr(finalArr);
+}
+
+function printFinalArr(finalArr) {
+  var text = '';
+  var newHTML = [];
+  _.each(finalArr, function(row) {
+    _.each(row, function(col) {
+      _.each(col, function(item){
+        newHTML.push('<span>' + item + ' ' + '</span>');
+      })
+      newHTML.push('<br />');
+    })
+    newHTML.push('<br />');
+  })
+  $('#resultArr').empty().append(newHTML);
 }
 
 function sumBinaryArrayRows(binnaryArray) {
